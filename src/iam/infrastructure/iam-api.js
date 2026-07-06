97c67b8868e1fa95f983df2e357602bedaca740d
Darioout7 <darfabavila@gmail.com>
2026-07-05 19:17:09 -0500

    fix(): fix link

--- src/iam/infrastructure/iam-api.js
+++ src/iam/infrastructure/iam-api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'https://flowboardwebservice.onrender.com';
+const API_BASE_URL = 'https://flowboardwebservice.onrender.com/api/v1/auth';
 
 export class IamApi {
 
